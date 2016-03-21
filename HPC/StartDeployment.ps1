ee00e20b877ab751672b3f185830fed9cbabd10b
Jesper Jensen <JJE@ATP.DK>
2016-03-21 21:17:47 +0100

    bug

--- HPC/StartDeployment.ps1
+++ HPC/StartDeployment.ps1
@@ -1,9 +1,10 @@
 ﻿#Deploy Azure HPC Cluster
-$RgName = "HPCtest"
-Login-AzureRmAccount -Credential Get-Credential
-New-AzureRmResourceGroup -Name $RgName
+$RgName = "AtpPi"
+$DeploymentName = "atppihpc"
+#Login-AzureRmAccount
+New-AzureRmResourceGroup -Name $RgName -Location "North Europe"
 
 $Time = [System.Diagnostics.Stopwatch]::StartNew()
-New-AzureRmResourceGroupDeployment -Name $RgName -ResourceGroupName $RgName -TemplateUri https://raw.githubusercontent.com/Azure/azure-quickstart-templates/master/create-hpc-cluster/ -TemplateParameterUri https://raw.githubusercontent.com/jjnetconn/Azure/master/HPC/azuredeploy.parameters.json
+New-AzureRmResourceGroupDeployment -Name $DeploymentName -ResourceGroupName $RgName -TemplateUri https://raw.githubusercontent.com/Azure/azure-quickstart-templates/master/create-hpc-cluster/azuredeploy.json -TemplateParameterUri https://raw.githubusercontent.com/jjnetconn/Azure/master/HPC/azuredeploy.parameters.json
 $Time.Stop
 $Time.Elapsed

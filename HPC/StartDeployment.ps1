0d6ede9853aa8f6fa67a2488762d7b7fea1c5918
Jesper Jensen <JJE@ATP.DK>
2016-03-21 21:05:32 +0100

    bugfix in param file

---
+++ HPC/StartDeployment.ps1
@@ -0,0 +1,9 @@
+﻿#Deploy Azure HPC Cluster
+$RgName = "HPCtest"
+Login-AzureRmAccount -Credential Get-Credential
+New-AzureRmResourceGroup -Name $RgName
+
+$Time = [System.Diagnostics.Stopwatch]::StartNew()
+New-AzureRmResourceGroupDeployment -Name $RgName -ResourceGroupName $RgName -TemplateUri https://raw.githubusercontent.com/Azure/azure-quickstart-templates/master/create-hpc-cluster/ -TemplateParameterUri https://raw.githubusercontent.com/jjnetconn/Azure/master/HPC/azuredeploy.parameters.json
+$Time.Stop
+$Time.Elapsed

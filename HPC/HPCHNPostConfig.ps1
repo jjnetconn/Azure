6b5bd49f9eb0e27b5543b69ca58098ca5c5adadb
Jesper Jensen <JJE@ATP.DK>
2016-03-21 20:17:38 +0100

    initial

---
+++ HPC/HPCHNPostConfig.ps1
@@ -0,0 +1,16 @@
+﻿Add-PSSnapin Microsoft.HPC
+
+while($true)
+{
+    Get-HpcNode -HealthState Unapproved -Name HPCDemoCN* | Assign-HpcNodeTemplate -Name "Default ComputeNode Template" -Confirm:$false
+    Get-HpcNode -State Offline -Name HPCDemoCN* | Set-HpcNodestate -State online -Confirm:$false
+    $onlineCNs = @(Get-HpcNode -State online -Name HPCDemoCN*)
+    if($onlineCNs.Count -eq 2)
+    {
+        break
+    }
+    else
+    {
+        Start-Sleep -Seconds 120
+    } 
+} 
